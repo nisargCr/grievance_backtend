6f05e13b36b14918bf5acec827d8e13eaf7758b1
datta491 <dattatreya.valeti@criticalriver.com>
2022-11-11 13:27:25 +0530

    changes  added

--- src/complaint/complaint.controller.js
+++ src/complaint/complaint.controller.js
@@ -43,7 +43,7 @@ exports.fileUpload = (req) => {
 exports.getComplaints = (req) => {
     try {
         return new Promise((resolve, reject) => {
-            complaintModel.find({})
+            complaintModel.find({}).sort({createdAt:-1})
                 .then(response => {
                     if (response){
                         resolve(response);
